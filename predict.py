9f8aab2a706fef5823b7a6746001ac94e5088c6e
dbratu777 <dab755@msstate.edu>
2025-02-06 14:29:07 -0600

    update filepaths and add conda environment requirements file

--- predict.py
+++ predict.py
@@ -4,13 +4,13 @@ from ultralytics import YOLO
 
 model = YOLO("custom.pt")
 
-process_dir = '/home/p3/code/yolo/test'
+process_dir = 'test'
 for filename in os.listdir(process_dir):
     image_path = os.path.join(process_dir, filename)
     if os.path.isfile(image_path):
         model.predict(source=image_path, save=True, save_txt=True, save_conf=True)
 
-results_dir = '/home/p3/code/yolo/runs/detect/predict'
+results_dir = 'runs/detect/predict'
 zip_file = f"{results_dir.rstrip(os.sep)}"
 shutil.make_archive(zip_file, 'zip', results_dir)
 shutil.rmtree(results_dir)
